cf41786f41b2b8556f585de17514520cf6589844
lanhuidong <472897576@qq.com>
2022-04-07 15:54:50 +0800

    feat: 定义一些空数组

---
+++ src/main/java/com/nexusy/jcc/misc/Arrays.java
@@ -0,0 +1,18 @@
+package com.nexusy.jcc.misc;
+
+/**
+ * @author lanhuidong
+ * @since 2022-04-07
+ */
+public final class Arrays {
+
+    public static final byte[] EMPTY_BYTES = {};
+    public static final int[] EMPTY_INTS = {};
+    public static final char[] EMPTY_CHARS = {};
+    public static final Object[] EMPTY_OBJECTS = {};
+    public static final String[] EMPTY_STRINGS = {};
+
+    private Arrays() {
+    }
+
+}
