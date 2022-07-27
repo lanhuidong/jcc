8d99a521ef3c32ced638a284cf2fdc3f2ceec248
lanhuidong <472897576@qq.com>
2022-07-27 09:36:37 +0800

    feat: 处理发布失败的问题

--- src/main/java/com/nexusy/jcc/misc/NumberUtil.java
+++ src/main/java/com/nexusy/jcc/misc/NumberUtil.java
@@ -8,6 +8,9 @@ public class NumberUtil {
 
     /**
      * 该方法主要是为了处理Integer.MIN_VALUE=-Integer.MIN_VALUE的问题
+     *
+     * @param number 原数字
+     * @return 返回非负数
      */
     public static int toPositiveNumber(int number) {
         if (number == Integer.MIN_VALUE) {
@@ -21,6 +24,9 @@ public class NumberUtil {
 
     /**
      * 该方法主要是为了处理Long.MIN_VALUE=-Long.MIN_VALUE的问题
+     *
+     * @param number 原数字
+     * @return 返回非负数
      */
     public static long toPositiveNumber(long number) {
         if (number == Long.MIN_VALUE) {
